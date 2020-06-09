30a04694ca95f0b452936816c20937e77d81d52a
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2020-06-09 17:59:34 +0200

    Many bugs are fixed
    will finish boss1 soon :)

---
+++ Endles-Runner/Assets/00-Game root/Scripts/GameplayScripts/Objects/Abstract Objects.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class PowerUps : Objects
+{
+    public virtual void Effect() { Destroy(gameObject); }
+    public override void CollisionEffect()
+    {
+        switch (PlayerResistance())
+        {
+            case 1: //action done if player has simple resistance
+                Effect();
+                break;
+
+            case 2: //action done if player has time-based resistance
+                Destroy(gameObject);
+                break;
+
+            default: //action done if player has no resistance
+                Effect();
+                break;
+        }
+    }
+    public override void IdleEffect()
+    {
+        transform.Rotate(Rotation);
+    }
+
+}
+
+public abstract class Collectable : Objects
+{
+    public virtual void Effect() { Destroy(gameObject); }
+    public override void CollisionEffect()
+    {
+        Effect();
+    }
+    public override void IdleEffect()
+    {
+        transform.Rotate(Rotation);
+    }
+}
+
+public abstract class Obstacle : Objects
+{
+    public virtual void Effect(bool simpleResistance) { Player.GetComponent<CharacterReact>().Die(true,true);}
+    public override void CollisionEffect()
+    { 
+        
+        switch (PlayerResistance())
+        {
+            case 1: //action done if player has simple resistance
+                Effect(true);
+                break;
+
+            case 2: //action done if player has time-based resistance
+                Destroy(gameObject);
+                break;
+
+            default: //action done if player has no resistance
+                Effect(false);
+                break;
+        }
+    }
+}
+
+public abstract class Trigger : Objects
+{
+    public virtual void Effect() { Destroy(gameObject); }
+    public virtual void BounceEffect() { }
+    public override void CollisionEffect()
+    {
+        switch (PlayerResistance())
+        {
+            case 1: //action done if player has simple resistance
+                Effect();
+                break;
+
+            case 2: //action done if player has time-based resistance
+                BounceEffect();
+                break;
+
+            default: //action done if player has no resistance
+                Effect();
+                break;
+        }
+    }
+
+}
+
+public abstract class World : MonoBehaviour
+{
+    void Update()
+    {
+        if (GameManager.characterDeath == false)
+        {
+            if (gameObject.transform.position.z < GameManager.Player.position.z - 6f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
+
+public abstract class Objects : World
+{
+    void Update()
+    {
+
+        IdleEffect();
+    }
+    public Vector3 Rotation;
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider collision) // All objects except obstacles,excluding hole
+    { 
+
+        Player = collision.gameObject;
+
+        CollisionEffect();
+    }
+
+    public virtual int PlayerResistance()
+    {
+        return Player.GetComponent<CharacterReact>().PlayerResistance();
+    }
+
+    public virtual void CollisionEffect() { }
+
+    public virtual void IdleEffect() { }
+
+
+}
+
+
+
+
---
+++ Endles-Runner/Assets/00-Game root/Scripts/GameplayScripts/Player/CharacterAbility.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAbility : MonoBehaviour
+{
+    CharacterStats stats;
+    CharacterReact React;
+    ParticleSystem em;
+    ParticleSystem.MainModule emSettings;
+
+    bool _charged = false;
+    void Start()
+    {
+        stats = GetComponentInChildren<CharacterStats>();
+        React = GetComponentInChildren<CharacterReact>();
+        em = GetComponentInChildren<ParticleSystem>();
+        emSettings = GetComponentInChildren<ParticleSystem>().main;
+    }
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.S)) && (_charged) && ((GameManager.CurrentLevel > 1) || (GameManager.CurrentLevel == 0 )))
+        {
+            Shoot();
+            stats.ChangePower(-100f);
+            Charged(false);
+        }
+    }
+
+    public void Shoot()
+    {
+        emSettings.startColor = React.CurrentMaterial().color;
+        em.Play(true);
+
+        if (em.isPlaying)
+        {
+            Kill();
+        }
+       
+    }
+
+    public void Charged(bool Charged)
+    {
+        _charged = Charged;
+    }
+
+    void Kill()
+    {
+        Vector3 pos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 5f);
+
+        foreach (Collider col in colliders)
+        {
+            if (col.gameObject.tag == "Lethal")
+            {
+                Destroy(col.gameObject);
+            }
+
+        }
+    }
+
+
+}
