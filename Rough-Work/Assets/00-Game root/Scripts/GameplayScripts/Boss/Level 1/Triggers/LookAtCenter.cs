d5af4ece73dc6a2ff0f07f7685e2234d34d156d5
TheAidaan <56072883+TheAidaan@users.noreply.github.com>
2020-06-10 16:17:25 +0200

    so very very close

---
+++ Rough-Work/Assets/00-Game root/Scripts/GameplayScripts/Boss/Level 1/Triggers/LookAtCenter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCenter : Trigger
+{
+    public override void Effect()
+    {
+        Player.transform.LookAt(transform.parent);
+        Debug.Log("looking");
+        base.Effect();
+    }
+}
